39e9a3e5ba561fe37dc2f575030f1875b81a3ac2
Shan33433 <1806489042@qq.com>
2024-07-19 21:48:05 +0800

    Update config.js

--- themes/landing/config.js
+++ themes/landing/config.js
@@ -1,21 +1,21 @@
 const CONFIG = {
 
-  LANDING_HEADER_BUTTON_1_TITLE: '茴香豆知识库',
-  LANDING_HEADER_BUTTON_1_LINK: 'https://www.huixiangdou.to',
+  LANDING_HEADER_BUTTON_1_TITLE: 'Github开源',
+  LANDING_HEADER_BUTTON_1_URL: 'https://github.com/tangly1024/NotionNext',
 
-  LANDING_HEADER_BUTTON_2_TITLE: '我们的公众号',
-  LANDING_HEADER_BUTTON_2_LINK: 'https://www.huixiangdou.to',
+  LANDING_HEADER_BUTTON_2_TITLE: '作者博客',
+  LANDING_HEADER_BUTTON_2_URL: 'https://blog.tangly1024.com/',
 
   // 首页大图英雄板块
-  LANDING_HERO_TITLE_1: '茴香豆知识库',
-  LANDING_HERO_P_1: '深度思考，跳出信息茧房，成为自己大脑的主人！',
+  LANDING_HERO_TITLE_1: 'NotionNext',
+  LANDING_HERO_P_1: '快速搭建独立站、轻松放大品牌价值！',
   LANDING_HERO_BUTTON_1_TEXT: '开始体验',
-  LANDING_HERO_BUTTON_1_LINK: 'https://www.huixiangdou.top',
+  LANDING_HERO_BUTTON_1_LINK: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next',
   LANDING_HERO_BUTTON_2_TEXT: '了解更多',
-  LANDING_HERO_BUTTON_2_LINK: 'https://www.huixiangdou.top',
+  LANDING_HERO_BUTTON_2_LINK: 'https://docs.tangly1024.com/about',
   LANDING_HERO_VIDEO_IMAGE: '/images/home.png',
   //   HERO_VIDEO_URL: '/videos/video.mp4',
-  LANDING_HERO_VIDEO_IFRAME: 'https://www.huixiangdou.to',
+  LANDING_HERO_VIDEO_IFRAME: '//player.bilibili.com/player.html?aid=913088616&bvid=BV1fM4y1L7Qi&cid=1187218697&page=1&&high_quality=1',
   LANDING_HERO_VIDEO_TIPS: 'Watch the full video (2 min)',
 
   // 特性介绍
