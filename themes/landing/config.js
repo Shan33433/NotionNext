0313df21d711ccd9ad05f1c2d213d45fa5bff2b3
Shan33433 <1806489042@qq.com>
2024-07-20 15:00:13 +0800

    Update config.js

--- themes/landing/config.js
+++ themes/landing/config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   LANDING_HERO_BUTTON_1_LINK: 'https://www.huixiangdou.top/',
   LANDING_HERO_BUTTON_2_TEXT: '了解更多',
   LANDING_HERO_BUTTON_2_LINK: 'https://www.huixiangdou.top/',
-  LANDING_HERO_VIDEO_IMAGE: '/images/home.png',
+  LANDING_HERO_VIDEO_IMAGE: '/f25c4b7531170149b1fb1ea34e4de4da.png',
   //   HERO_VIDEO_URL: '/videos/video.mp4',
   LANDING_HERO_VIDEO_IFRAME: '//player.bilibili.com/player.html?aid=913088616&bvid=BV1fM4y1L7Qi&cid=1187218697&page=1&&high_quality=1',
   LANDING_HERO_VIDEO_TIPS: 'Watch the full video (2 min)',
